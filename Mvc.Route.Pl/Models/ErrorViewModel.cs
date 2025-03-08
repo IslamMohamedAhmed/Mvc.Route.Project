74dabf16842cd0bc0e65bf575f8c36def00cd1e8
pc <IslamMohamedAhmed>
2025-03-08 15:20:15 +0200

    Add project files.

---
+++ Mvc.Route.Pl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mvc.Route.Pl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
